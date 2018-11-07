0f807e9e86c27a21ec430259ee6999cd4d8ec183
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    chef: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- device.mk
+++ device.mk
@@ -22,9 +22,7 @@ $(call inherit-product, vendor/motorola/chef/chef-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
