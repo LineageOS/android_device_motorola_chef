c5ff9d6d10ce36044c471a970b7c9b11e42798d4
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-27 01:35:39 -0500

    chef: Restructure tree to match nash
    Change-Id: Ibd9953004a13f8264897a4c8e090448cd96eb58b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,26 +24,26 @@ include device/motorola/msm8998-common/BoardConfigCommon.mk
 DEVICE_PATH := device/motorola/chef
 
 # A/B partitions
-AB_OTA_UPDATER := true
-
 AB_OTA_PARTITIONS += \
     boot \
     system \
     vendor
 
+AB_OTA_UPDATER := true
+
 # Assertions
-TARGET_BOARD_INFO_FILE := device/motorola/chef/board-info.txt
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := chef
 
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/chef_manifest.xml
+
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/c0c4000.sdhci
 TARGET_KERNEL_CONFIG := lineageos_chef_defconfig
-
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/chef_manifest.xml
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
