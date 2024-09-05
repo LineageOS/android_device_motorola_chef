15ffddb782b44e485cc3d319d52938027231bb08
Nolen Johnson <johnsonnolen@gmail.com>
2024-09-05 01:45:34 -0400

    fixup! chef: Sync extract scripts with templates
    Change-Id: I19da0a4d464d323dddfa8fd07521838cbeba79d0

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -6,6 +6,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
 export DEVICE=chef
